c580607694abc11b23a87e413de31ce1fe98fb52
BabisDigos <ntigos2001@gmail.com>
2022-12-02 13:40:34 +0200

    Improved version

--- Day2/index.ts
+++ Day2/index.ts
@@ -1,5 +1,25 @@
 import * as fs from "fs";
 
+type Player = Record<string, number>;
+
+const handScore: Player = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+const turnScore: Record<string, Player> = {
+  A: { X: 3, Y: 6, Z: 0 },
+  B: { X: 0, Y: 3, Z: 6 },
+  C: { X: 6, Y: 0, Z: 3 },
+};
+
+const cheatStrategy: Player = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+};
+
 const file = fs.readFileSync("input.txt", "utf-8");
 
 const getRounds = (file: string): string[][] => {
@@ -11,84 +31,24 @@ const getRounds = (file: string): string[][] => {
   return turn;
 };
 
-const getRoundWinnerPt1 = (round: string[]): number[] => {
-  if (round[0] === "A") {
-    if (round[1] === "X") return [3, 3];
-    else if (round[1] === "Y") return [0, 6];
-    else if (round[1] === "Z") return [6, 0];
-  } else if (round[0] === "B") {
-    if (round[1] === "X") return [6, 0];
-    else if (round[1] === "Y") return [3, 3];
-    else if (round[1] === "Z") return [0, 6];
-  } else {
-    if (round[1] === "X") return [0, 6];
-    else if (round[1] === "Y") return [6, 0];
-    else if (round[1] === "Z") return [3, 3];
-  }
-  return [0, 0];
+const calculateScorePt1 = (round: string[][]): number => {
+  return round
+    .map(([a, b]) => turnScore[a][b] + handScore[b])
+    .reduce((a, b) => a + b, 0);
 };
 
-const getRoundWinnerPt2 = (round: string[]): number[] => {
-  if (round[1] === "X") return [6, 0];
-  else if (round[1] === "Y") return [3, 3];
-  else if (round[1] === "Z") return [0, 6];
-  return [0, 0];
+const calculateScorePt2 = (round: string[][]): number => {
+  return round
+    .map(([a, b]) => {
+      let cheatB = Object.entries(turnScore[a])
+        .filter(([key, value]) => value === cheatStrategy[b])
+        .flat()[0] as string;
+      return turnScore[a][cheatB] + handScore[cheatB];
+    })
+    .reduce((a, b) => a + b, 0);
 };
 
-const calculateScorePt1 = (rounds: string[][]): number[] => {
-  let player1Score: number = 0;
-  let player2Score: number = 0;
+const rounds: string[][] = getRounds(file);
 
-  rounds.map((round) => {
-    if (round[0] === "A") player1Score += 1;
-    else if (round[0] === "B") player1Score += 2;
-    else if (round[0] === "C") player1Score += 3;
-
-    if (round[1] === "X") player2Score += 1;
-    else if (round[1] === "Y") player2Score += 2;
-    else if (round[1] === "Z") player2Score += 3;
-
-    const roundWinner: number[] = getRoundWinnerPt1(round);
-    player1Score += roundWinner[0];
-    player2Score += roundWinner[1];
-  });
-
-  return [player1Score, player2Score];
-};
-
-const calculateScorePt2 = (rounds: string[][]): number[] => {
-  let player1Score: number = 0;
-  let player2Score: number = 0;
-
-  rounds.map((round) => {
-    if (round[0] === "A") {
-      player1Score += 1;
-      if (round[1] === "X") player2Score += 3;
-      else if (round[1] === "Y") player2Score += 1;
-      else if (round[1] === "Z") player2Score += 2;
-    } else if (round[0] === "B") {
-      player1Score += 2;
-      if (round[1] === "X") player2Score += 1;
-      else if (round[1] === "Y") player2Score += 2;
-      else if (round[1] === "Z") player2Score += 3;
-    } else {
-      player1Score += 3;
-      if (round[1] === "X") player2Score += 2;
-      else if (round[1] === "Y") player2Score += 3;
-      else if (round[1] === "Z") player2Score += 1;
-    }
-
-    const roundWinner: number[] = getRoundWinnerPt2(round);
-    player1Score += roundWinner[0];
-    player2Score += roundWinner[1];
-  });
-
-  return [player1Score, player2Score];
-};
-
-const roundsPt1: string[][] = getRounds(file);
-const roundsPt2: string[][] = getRounds(file);
-
-console.log("Part 1: ", calculateScorePt1(roundsPt1));
-console.log("Part 2: ", calculateScorePt2(roundsPt2));
-
+console.log("Part 1:", calculateScorePt1(rounds));
+console.log("Part 2:", calculateScorePt2(rounds));
