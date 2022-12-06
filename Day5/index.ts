e9e7f8f7f46d64e9806f35b47c4ce3486cb7782d
BabisDigos <ntigos2001@gmail.com>
2022-12-06 18:35:15 +0200

    This is so goooood

--- Day5/index.ts
+++ Day5/index.ts
@@ -1,52 +1,66 @@
 import * as fs from "fs";
 
-const file = fs.readFileSync("example.txt", "utf-8").split(/\r?\n/);
+const file = fs.readFileSync("input.txt", "utf-8").split(/\r?\n/);
 
 const containers = file.slice(0, file.indexOf(""));
 
-const countContainers = containers.at(-1)?.replace(/\s/g, "").length;
+const countContainers = containers.pop()?.replace(/\s/g, "").length;
 
 const moves = file.slice(file.indexOf("") + 1, file.length);
 
-// const getStack = (containers: string[]): string[][] => {
-//   let neww = containers
-//     .map((container) => {
-//       return container.split("   ").filter((x) => x.split(" "));
-//     })
-//     .map((x) =>
-//       x
-//         .map((y) => y.replace(/[ ]/g, "").replace(/[\]]+/g, "],").split(","))
-//         .flat()
-//     );
-//   neww.pop();
-//   neww.map((x) => x.pop());
-//   return neww;
-// };
+const initStack = (
+  containers: string[],
+  totalContainers: number
+): string[][] => {
+  let stacks: string[][] = [];
+  let stack = [...containers].reverse();
 
-// const oof = getStack(containers);
-// console.log(oof);
-
-containers.pop();
-const stack = containers.reverse();
-
-console.log(stack);
-
-const stacks: string[][] = [];
-
-for (let i = 0; i < countContainers!; i++) {
-  stacks.push(
-    // stack.map((x) => x.slice(i * 3, i * 3 + 3).replace(/\s/g, ""))
+  for (let i = 0; i < totalContainers; i++) {
     i === 0
-      ? stack.map((x) => x.slice(0, 3).replace(/\s/g, ""))
-      : stack.map((x) => x.slice(i * 3 + 1, i * 4 - 1).replace(/\s/g, ""))
+      ? stacks.push(stack.map((x) => x.slice(0, 3).replace(/\s/g, "")))
+      : stacks.push(
+          stack.map((x) => x.slice(i * 4 - 1, i * 4 + 3).replace(/\s/g, ""))
+        );
+  }
+  return stacks.map((x) => x.filter((y) => y !== ""));
+};
+
+const initMoves = (moves: string[]): number[][] => {
+  return moves.map((move) =>
+    move
+      .split(" ")
+      .filter((x) => +x)
+      .map(Number)
   );
-}
+};
 
-// stack.map((x) => {
-//   stacks.push([x.slice(0, 3).replace(/\s/g, "")]);
-//   stacks.push([x.slice(3, 7).replace(/\s/g, "")]);
-//   stacks.push([x.slice(7, 11).replace(/\s/g, "")]);
-//   // stacks[2].push(x.slice(7, 11).replace(/\s/g, ""));
-// });
+const getResult = (stacks: string[][]): string => {
+  return stacks.reduce((result, stack) => result + stack.at(-1)?.at(-2), "");
+};
 
-console.log(stacks);
+const solvePt1 = (stacks: string[][], moves: number[][]): string => {
+  moves.forEach(([num, from, to]) => {
+    for (let i = 0; i < num; i++) {
+      stacks[to - 1].push(stacks[from - 1].pop()!);
+    }
+  });
+  return getResult(stacks);
+};
+
+const solvePt2 = (stacks: string[][], moves: number[][]): string => {
+  moves.forEach(([num, from, to]) => {
+    let tmp: string[] = [];
+    for (let i = 0; i < num; i++) {
+      tmp.unshift(stacks[from - 1].pop()!);
+    }
+    stacks[to - 1].push(...tmp);
+  });
+  return getResult(stacks);
+};
+
+const movesArr: number[][] = initMoves(moves);
+const s = initStack(containers, countContainers!);
+const s2 = initStack(containers, countContainers!);
+
+console.log("Part 1:", solvePt1(s, movesArr));
+console.log("Part 2:", solvePt2(s2, movesArr));
