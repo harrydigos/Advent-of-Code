9aae828e2b63a3b15642e7a470eb8dc43a7636ec
BabisDigos <ntigos2001@gmail.com>
2022-12-11 16:40:25 +0200

    Both parts

--- Day10/index.ts
+++ Day10/index.ts
@@ -1,6 +1,8 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
 type Command = { type: "noop"; cycles: number } | { type: "addx"; value: number; cycles: number };
+
+type Crt = "." | "#" | "";
 
 const readCommands = (): Command[] => {
   return readFileSync("input.prod", "utf-8")
@@ -19,18 +21,32 @@ const execCycle = (command: Command, commands: Command[]) => {
   }
 };
 
+const selectCrtRow = (cycle: number): number => {
+  return Math.floor((cycle - 1) / 40);
+};
+
 const commands: Command[] = readCommands();
 let registerX = 1;
 let cycleCount = 1;
 let result: number[] = [];
 
+var crt: Crt[][] = Array.from({ length: 6 }, () => new Array(40));
+
 while (commands.length) {
-  execCycle(commands[0], commands);
+  let row = selectCrtRow(cycleCount);
+  let currCycle = cycleCount % 40;
+  let spritePos = registerX;
+
+  if ([spritePos, spritePos + 1, spritePos + 2].includes(currCycle)) crt[row].push("#");
+  else crt[row].push(".");
+
   cycleCount++;
+  execCycle(commands[0], commands);
   if (cycleCount === 20 || (cycleCount > 40 && (cycleCount - 20) % 40 === 0)) result.push(registerX * cycleCount);
 }
 
-console.log(
-  "Part 1:",
-  result.reduce((a, b) => a + b, 0)
-);
+// Part 1
+console.log(result.reduce((a, b) => a + b, 0));
+
+// Part 2
+writeFileSync("output.prod", crt.map((row) => row.join("")).join("\n"));
